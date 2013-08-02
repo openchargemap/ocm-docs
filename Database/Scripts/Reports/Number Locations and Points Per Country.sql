06a5b46364a77c92bd325bdb79df6520d9632575
webprofusion-chrisc <christopher.cook@webprofusion.com>
2013-08-02 12:34:15 +0800

    SQL Scripts

---
+++ Database/Scripts/Reports/Number Locations and Points Per Country.sql
@@ -0,0 +1,19 @@
+SELECT 
+	COUNT(*) AS NumLocations, 
+	SUM(CASE WHEN NumberOfPoints=0 THEN 1 ELSE ISNULL(NumberOfPoints,1) END)  as NumPoints, 
+	SUM(TmpConnections.NumConnections) as NumConnections,
+	Country.Title as Country 
+FROM ChargePoint 
+INNER JOIN AddressInfo ON AddressInfo.ID=ChargePoint.AddressInfoID
+INNER JOIN Country ON Country.ID=AddressInfo.CountryID
+LEFT JOIN 
+(
+	SELECT SUM(CASE WHEN Quantity=0 THEN 1 ELSE ISNULL(Quantity,1) END) as NumConnections, 
+	ChargePointID FROM ConnectionInfo GROUP BY ChargePointID) as TmpConnections
+ON TmpConnections.ChargePointID=ChargePoint.ID
+WHERE ChargePoint.SubmissionStatusTypeID IN(100,200)
+GROUP BY Country.Title
+ORDER BY NumLocations DESC
+
+
+select * from ConnectionInfo
